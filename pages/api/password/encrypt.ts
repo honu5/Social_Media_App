f0594668f6b461aec7349ebe11aae105bdd30bc1
Honelgn Yohannes <liranso392@gmail.com>
2026-02-26 15:08:03 +0300

    The encryption made mor secured

--- pages/api/password/encrypt.ts
+++ pages/api/password/encrypt.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import cors from "server/helpers/cors";
 import { encrypt } from "server/helpers/password";
 import initBodyParser from "server/helpers/initBodyParser";
-
+//secure encryption
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   initBodyParser();
   await cors(req, res);
